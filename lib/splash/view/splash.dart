b9d9762b90c906bd53f1d07fc6560305f1b0adc0
pranavpv773 <pranavpv773@gmail.com>
2022-08-09 19:33:20 +0530

    splash started

---
+++ lib/splash/view/splash.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
