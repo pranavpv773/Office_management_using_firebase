41894a71858e084445093469118cfd66cd118c68
pranavpv773 <pranavpv773@gmail.com>
2022-08-10 18:55:07 +0530

    home page partially created

--- lib/login/view_model/home_provider.dart
+++ lib/login/view_model/home_provider.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:user_management_app/home/view/home_page.dart';
 import 'package:user_management_app/sign_up/view/sign_up.dart';
 
 class LoginProvider with ChangeNotifier {
   final userName = TextEditingController();
   final password = TextEditingController();
-  onTabFunction(BuildContext context) {
+  onTabLoginFunction(BuildContext context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (ctx) {
+          return const UserHomeScreen();
+        },
+      ),
+    );
+  }
+
+  onTabGoogleFunction(BuildContext context) {
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(
         builder: (ctx) {
