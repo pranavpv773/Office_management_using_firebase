a33c7447be467cb232709a08e34e7b29cd85690a
pranavpv773 <pranavpv773@gmail.com>
2022-08-10 19:15:53 +0530

    conflict cleared

---
+++ lib/login/view/widgets/social_icons.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SocialIcons extends StatelessWidget {
+  const SocialIcons({Key? key, required this.primary, required this.text})
+      : super(key: key);
+  final Color primary;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 30,
+        ),
+        primary: primary,
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            30,
+          ),
+        ),
+      ),
+      onPressed: (() {
+        // checkLogin(context);
+      }),
+      child: Text(
+        text,
+      ),
+    );
+  }
+}
